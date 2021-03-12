31353bb908bd24b081faee09418a23c936079452
carooobregon <carooobregon@gmail.com>
2021-03-11 21:28:45 -0700

    refactored rply grammar

--- rPLY/parser.py
+++ rPLY/parser.py
@@ -32,24 +32,18 @@ class Parser():
         def expression_parens(p):
             return Termino()
 
-        @self.pg.production('varsAuxA : ID varsAuxB')
+        @self.pg.production('varsAuxA : ID COMMA varsAuxA')
+        @self.pg.production('varsAuxA : ID')
         def expression_parens(p):
             return Termino()
 
-        @self.pg.production('varsAuxB : COMMA varsAuxA')
-        @self.pg.production('varsAuxB : none')
+        @self.pg.production('bloque : OPEN_CURLY estatuto CLOSE_CURLY')
         def expression_parens(p):
             return Termino()
 
-        @self.pg.production('bloque : OPEN_CURLY estatuto bloqueAux CLOSE_CURLY')
-        def expression_parens(p):
-            return Termino()
-
-        @self.pg.production('bloqueAux : estatuto bloqueAux')
-        @self.pg.production('bloqueAux : none')
-        def expression_parens(p):
-            return Termino()
-
+        @self.pg.production('estatuto : condicion estatuto')
+        @self.pg.production('estatuto : asignacion estatuto')
+        @self.pg.production('estatuto : escritura estatuto')
         @self.pg.production('estatuto : escritura')
         @self.pg.production('estatuto : asignacion')
         @self.pg.production('estatuto : condicion')
@@ -65,13 +59,10 @@ class Parser():
         def expression_parens(p):
             return Termino()
 
-        @self.pg.production('escrHelperA : expresion escrHelperB')
-        @self.pg.production('escrHelperA : STRING escrHelperB')
-        def expression_parens(p):
-            return Termino()
-
-        @self.pg.production('escrHelperB : COMMA escrHelperA')
-        @self.pg.production('escrHelperB : none')
+        @self.pg.production('escrHelperA : expresion COMMA escrHelperA')
+        @self.pg.production('escrHelperA : STRING COMMA escrHelperA')
+        @self.pg.production('escrHelperA : STRING')
+        @self.pg.production('escrHelperA : expresion')
         def expression_parens(p):
             return Termino()
 
@@ -86,28 +77,18 @@ class Parser():
         def expression_parens(p):
             return Termino()
 
+        @self.pg.production('exp : termino PLUS exp')
+        @self.pg.production('exp : termino MINUS exp')
         @self.pg.production('exp : termino')
-        @self.pg.production('exp : termino expHelper')
         def expression_parens(p):
             return Termino()
 
-        @self.pg.production('expHelper : PLUS exp')
-        @self.pg.production('expHelper : MINUS exp')
-        @self.pg.production('expHelper : none')
-        def expression_parens(p):
-            return Termino()
-
+        @self.pg.production('termino : factor MUL termino')
+        @self.pg.production('termino : factor DIV termino')
         @self.pg.production('termino : factor')
-        @self.pg.production('termino : factor fAux')
         def expression_parens(p):
             return Termino()
-
-        @self.pg.production('fAux : MUL termino')
-        @self.pg.production('fAux : DIV termino')
-        @self.pg.production('fAux : none')
-        def expression_parens(p):
-            return Termino()
-
+            
         @self.pg.production('factor : OPEN_PARENS expresion CLOSE_PARENS')
         @self.pg.production('factor : PLUS var_cte')
         @self.pg.production('factor : MINUS var_cte')
