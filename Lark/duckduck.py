e1da676b2141fe0b91fed68b9a59ade2430a077a
carooobregon <carooobregon@gmail.com>
2021-03-11 21:05:28 -0700

    added test files

--- Lark/duckduck.py
+++ Lark/duckduck.py
@@ -1,10 +1,10 @@
 from lark import Lark, Transformer, v_args
 
 grammar = """
-    start : programa 
     programa : PROGRAM ID SEMICOLON progaux bloque
     progaux : vars | estatuto
-    vars : VAR ID COLON tipo SEMICOLON
+    vars : VAR varaux COLON tipo SEMICOLON
+    varaux : ID COMMA varaux | ID
     bloque : OPEN_CURLY estatuto CLOSE_CURLY
     estatuto : asignacion estatuto
                 | escritura estatuto
@@ -15,13 +15,12 @@ grammar = """
     asignacion : ID EQUALS expression SEMICOLON
     condicion : IF OPEN_PARENS expression CLOSE_PARENS bloque condhelper
     condhelper : SEMICOLON | ELSE bloque SEMICOLON
-    expression : exp LEFTOP exp | exp RIGHTOP exp | exp
+    expression : exp LEFTOP exp | exp RIGHTOP exp | exp NEQ exp | exp
     exp : termino | termino PLUS exp | termino MINUS exp
     termino : factor | factor MUL termino | factor DIV termino
     factor : PLUS var_cte | MINUS var_cte | var_cte | OPEN_PARENS expression CLOSE_PARENS
     tipo : INT | FLOAT
     var_cte : ID | CTE_FLOAT | CTE_INT | COLON
-
     MUL : "*"
     PRINT : "print"
     INT : "int"
@@ -43,6 +42,7 @@ grammar = """
     OPEN_CURLY : "{"
     CLOSE_CURLY : "}"
     COMMA : ","
+    NEQ : "<>"
 
     %import common.ESCAPED_STRING   -> STRING
     %import common.SIGNED_INT    -> CTE_INT
@@ -54,53 +54,22 @@ grammar = """
     %ignore WS
     %ignore WS_INLINE
     %ignore " "
-
 """
+def inputUser():
+     # archName = input("Enter file name with extension: \n")
+     file = open("correct.txt", "r", encoding="utf-8")
+     user_input = file.read()
+     return user_input
 
 def main():
-     test()
-     
-def test():
-    #  input = '{print("hola"); ab = 3*2; if(2 > 2){ ab = 2*2; }else{print(2);}; print("hola");}'
-     correct_input = """
-          program ab;
-          var ab : int;
-          {
-               print("hola");
-               ab = 3 * 2 + 1 * (2 + 2);
-               if(2 > 2){
-                    ab = 2*2;
-               } else {
-                    print(2);
-               };
-               print("hola", ab, 2 * 2, 1);
-          }
-     """
-     incorrect_input = """
-          program ab;
-          var ab : int;
-          
-               print("hola");
-               ab = 3 * 2 + 1 * (2 + 2);
-               if(2 > 2){
-                    ab = 2*2;
-               } else {
-                    print(2);
-               };
-               print("hola", ab, 2 * 2, 1);
-          }
-     """
-     calc_parser = Lark(grammar, parser='lalr')
+     code = inputUser()
+     calc_parser = Lark(grammar, parser='lalr', start="programa")
      calc = calc_parser.parse
-    #  parser = Lark(gramatica,start = "programa")
      try:
-          if(calc(correct_input)):
-               print("Programa Valido")
+          # print(calc(code))
+          print("Programa Valido")
      except Exception as ex:
-          print("parsing failed")
-     # print(parser.parse(test_lilduck1).pretty())
-     # print(parser.parse(test_lilduck2).pretty())
-
+          print("Programa Invalido")
 
 if __name__=='__main__':
-     main()# Stack example
+     main()
